8aa6df36e4acd800e1512c70a3a1434c0af6f8af
K4ugummi <stephan.schauerte@gmail.com>
2018-10-17 20:57:47 +0200

    Add uebung 1_4_a

---
+++ src/Factorial.java
@@ -0,0 +1,17 @@
+// Calculate factorial of static final long NUMBER.
+public class Factorial {
+
+    static final long NUMBER = 40;
+
+    public static void main(String[] args) {
+
+        long factorial = 1;
+
+        for (long currentFac = 1; currentFac <= NUMBER; currentFac++) {
+            factorial = factorial * currentFac;
+        }
+
+        System.out.println("Factorial of " + NUMBER + " = " + factorial);
+    }
+
+} // End of class Factorial
